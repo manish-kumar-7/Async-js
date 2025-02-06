2c422a67979cfcb75109ad3636bcbf7638524456
manish-kumar-7 <manishkumar28112005@gmail.com>
2025-02-06 21:07:37 +0530

    Add files via upload

---
+++ randomBackground.js
@@ -0,0 +1,23 @@
+const randomColor=function (){
+    const hex="0123456789ABCDEF";
+    let color="#";
+    for(let i=0;i<6;i++){
+        color+=hex[Math.floor(Math.random()*16)];
+    }
+return color;   
+}
+let set
+let s=document.querySelector('#start')
+let st=document.querySelector('#stop')
+let changeColor=function (){
+    function setcolor2  (){
+    document.body.style.backgroundColor=randomColor()
+    }
+    set=setInterval(setcolor2,2000)
+}
+let stopChangeColor=function (){
+    clearInterval(set)
+    set=null;
+}
+s.addEventListener("click",changeColor)
+st.addEventListener("click",stopChangeColor)
